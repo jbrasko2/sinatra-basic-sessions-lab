b0a702c8dc0c0673f02cd06810a2407c4c991cdd
jbrasko <jbrasko0508@gmail.com>
2020-12-02 15:46:24 -0600

    commit tree 52f463613626efe8234f71fa83a32078df5d711a

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "23409jf029m4f09234jrf0924f"
+      end
+      
+      get '/' do
+        erb :index
+      end
+
+      post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+      end
+
+
 end
